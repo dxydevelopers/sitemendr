b2522ad689e0d456d84e2a4a4979bae9228c71b0
dxydevelopers <dxydevelopers@gmail.com>
2026-06-18 10:34:56 +0300

    final

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -50,7 +50,7 @@ const legalLinks = [
 ];
 
 const contactLinks = [
-  { name: 'support@sitemendr.com', href: 'mailto:dxydevelopers@gmail.com', icon: Mail },
+  { name: 'dxydevelopers@gmail.com', href: 'mailto:dxydevelopers@gmail.com', icon: Mail },
   { name: '+254 140 122 685', href: 'tel:+254140122685', icon: Phone },
   { name: 'Sitemendr Tech, Nairobi, Kenya', href: 'https://www.google.com/maps/search/Nairobi+Kenya', icon: MapPin },
 ];
